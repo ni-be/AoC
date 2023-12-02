101c3167b612223972328e96840964394be95b08
Nikolas Bertrand <code@ni-be.dev>
2023-12-02 14:08:49 +0100

    Day2 AoC completed

---
+++ day2/day2.py
@@ -0,0 +1,84 @@
+# Day 2 Cube Conundrum
+
+COLORS = [12, 13, 14]
+
+
+def day2():
+    id_sum = 0
+    failed_game = []
+    with open('puzzle_input.txt', 'r') as file:
+        for line in file:
+            game = int(game_id(line))
+            set = find_sets(line)
+            # print(set)
+            for draw in set:
+                result = draw_split(draw)
+                for balls in result:
+                    game_draw = [game, 0, 0, 0]
+                    ball_split = number_balls(balls)
+                    if ball_split[0] == "red":
+                        game_draw[1] = ball_split[1]
+                    elif ball_split[0] == "green":
+                        game_draw[2] = ball_split[1]
+                    elif ball_split[0] == "blue":
+                        game_draw[3] = ball_split[1]
+                    failed_id = filter_games(game_draw)
+                    if failed_id not in failed_game:
+                        failed_game.append(failed_id)
+
+            if game not in failed_game:
+                id_sum += game
+    print(id_sum)
+
+
+def filter_games(game):
+    if game[1] > COLORS[0] or game[2] > COLORS[1] or game[3] > COLORS[2]:
+        id = game[0]
+        return id
+
+
+def number_balls(balls):
+    ball_split = ['0', '0']
+    string = ''
+    number = ''
+    for char in balls:
+        if not char.isdigit() and not char.isspace():
+            string += char
+        elif not char.isspace() and not char.isalpha():
+            number += char
+    ball_split[0] = string
+    ball_split[1] = int(number)
+    return ball_split
+
+
+def draw_split(draw):
+    draw_split = []
+    for i in range(3):
+        try:
+            draw_split.append(draw.split(",")[i])
+        except IndexError:
+            continue
+    return draw_split
+
+
+def find_sets(line):
+    contents = line.split(":")[1]
+    set = []
+    for i in range(10):
+        try:
+            set.append(contents.split(";")[i])
+        except IndexError:
+            continue
+    return set
+
+
+def game_id(line):
+    gid = ''
+    id = line.split(":")[0]
+    for char in id:
+        if char.isdigit():
+            gid += char
+    return gid
+
+
+day2()
