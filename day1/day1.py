0ad0802907ff9d8df5baf9ad57369405746dcfea
Nikolas Bertrand <code@ni-be.dev>
2023-12-03 20:29:23 +0100

    day 1 and 2 part2

--- day1/day1.py
+++ day1/day1.py
@@ -1,29 +1,13 @@
 # AoC2023 day 1
-
+import linecache
 # read txt line by line
 # extract first and last number of string
 # if there is only 1 number i.e. 7 it will equal 77
 # sum the numbers for each line
 
 NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-# total = 0
-# with open('puzzle_input.txt', 'r') as file:
-#     for line in file:
-#         x = []
-#         for char in line:
-#             if char in numbers:
-#                 if len(x) < 2:
-#                     x.append(char)
-#                 else:
-#                     x[1] = char
-#         if len(x) > 1:
-#             line_sum = x[0] + x[1]
-#         else:
-#             line_sum = x[0] + x[0]
-#         total += int(line_sum)
-# print(total)
-
-# clean up version
+WORDS = {'one': 1, 'two': 2, 'three': 3, 'four': 4,
+         'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
 
 def calibration_values(input):
@@ -55,4 +39,22 @@ def summarizer(line_values):
     return line_sum
 
 
+def part2(input):
+    lines = linecache.getlines(input)
+    # print(lines)
+    result = 0
+    for ln in lines:
+        # print(ln)
+        digits = []
+        for i in range(len(ln)):
+            if '0' <= ln[i] <= '9':
+                digits.append(int(ln[i]))
+            for k, number in WORDS.items():
+                if ln[i:i+len(k)] == k:
+                    digits.append(number)
+        result += digits[0]*10 + digits[-1]
+    print(result)
+
+
 calibration_values('puzzle_input.txt')
+part2('puzzle_input.txt')
--- day2/day2.py
+++ day2/day2.py
@@ -6,6 +6,7 @@ COLORS = [12, 13, 14]
 def day2():
     id_sum = 0
     failed_game = []
+    part_two = []
     with open('puzzle_input.txt', 'r') as file:
         for line in file:
             game = int(game_id(line))
@@ -16,19 +17,41 @@ def day2():
                 for balls in result:
                     game_draw = [game, 0, 0, 0]
                     ball_split = number_balls(balls)
+                    # print(ball_split)
                     if ball_split[0] == "red":
                         game_draw[1] = ball_split[1]
                     elif ball_split[0] == "green":
                         game_draw[2] = ball_split[1]
                     elif ball_split[0] == "blue":
                         game_draw[3] = ball_split[1]
+                    part_two.append(game_draw)
                     failed_id = filter_games(game_draw)
                     if failed_id not in failed_game:
                         failed_game.append(failed_id)
-
             if game not in failed_game:
                 id_sum += game
     print(id_sum)
+    # add part2
+    day2_two(part_two)
+
+
+def day2_two(data):
+    store = []
+    sub = [0, 0, 0, 0]
+    for draws in data:
+        if sub[0] != draws[0] and sub[0] != 0:
+            store.append(sub)
+            sub = [0, 0, 0, 0]
+
+        for i in range(4):
+            if sub[i] <= draws[i]:
+                sub[i] = draws[i]
+    store.append(sub)
+
+    sum = 0
+    for game in store:
+        sum += game[1]*game[2]*game[3]
+    print(sum)
 
 
 def filter_games(game):
@@ -69,7 +92,9 @@ def find_sets(line):
             set.append(contents.split(";")[i])
         except IndexError:
             continue
-    return set
+
+    set2 = [item.strip("\n") for item in set]
+    return set2
 
 
 def game_id(line):
